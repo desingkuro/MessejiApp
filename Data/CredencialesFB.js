5c69bd722c076a2cac456b0e24545cf2c5a3f7f9
desingkuro <andresmauricio745@gmail.com>
2024-06-03 20:00:42 -0500

    conexion a base de datos

---
+++ Data/CredencialesFB.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwDauNh_tBYvrVVBjN77ul1HUKvg2MlGM",
+  authDomain: "messeji-f0314.firebaseapp.com",
+  projectId: "messeji-f0314",
+  storageBucket: "messeji-f0314.appspot.com",
+  messagingSenderId: "267899754222",
+  appId: "1:267899754222:web:a0ec9715911998f54245ff"
+};
+
+
+// Initialize Firebase
+export const appFireBase = initializeApp(firebaseConfig);
--- context/ContextoGeneral.js
+++ context/ContextoGeneral.js
@@ -1,8 +1,12 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 import { FontAwesome5 } from '@expo/vector-icons';
 import { creacionUsuarioScreen } from "../Styles/creacionDeUsuarioStyle";
+import { appFireBase } from "../Data/CredencialesFB";
+import * as firebase from 'firebase/firestore'
+export const Contexto = createContext();
 
-export const Contexto = createContext()
+const db = firebase.getFirestore(appFireBase);
+const docRef = firebase.doc(db, "Usuarios", "users");
 
 function ContextoGeneral({children}) {
     const iconoUsuario = <FontAwesome5 name="user" size={84} color="black"/>
@@ -11,6 +15,25 @@ function ContextoGeneral({children}) {
     const [imgSelecionada,setImgUsuarioSelecionada] = useState(false);
     const [nombreUsuario,setUsuario] = useState('');
     const [loginactivo,setLoginActivo] = useState(false);
+
+    useEffect(()=>{
+      // Obtener el documento
+      async function getDocument(){
+        try {
+          const docSnap = await firebase.getDoc(docRef);
+          if (docSnap.exists()) {
+            // El documento existe, puedes acceder a sus datos
+            const data = docSnap.data();
+            console.log("Datos del documento:", data);
+          } else {
+            console.log("El documento no existe.");
+          }
+        } catch (error) {
+          console.error("Error al obtener el documento:", error);
+        }
+      }
+      getDocument()
+    },[])
 
   return (
     <Contexto.Provider value={{
